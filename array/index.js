f3bc9f60061f9218c1954ea730391e7df90e0d80
hakobyanviktoria8 <hakobyanviktorya8@gmail.com>
2023-03-16 22:52:33 +0400

    added array type

---
+++ array/index.js
@@ -0,0 +1,25 @@
+"use strict";
+const skills = ["dev", "qa", "devops"];
+for (const item of skills) {
+    console.log(item.toUpperCase());
+}
+let newArr = skills
+    .filter((x) => x !== "devops")
+    .map((x) => x + "! ")
+    .reduce((a, b) => a + b);
+console.log(newArr);
+const nums = [1, 2, 3, 434, 4, 5, 6, 6, 565, 34, 656, 56];
+let newNums = nums.reduce((a, b) => a + b);
+console.log(newNums);
+const users = ["Anna", "Asd", "Ajed"];
+let asd = users.map((x) => x + "11");
+let asdf = asd.map((x, idx) => {
+    return {
+        id: idx,
+        name: x,
+    };
+});
+console.log(asdf);
+const logs = [12, 32, "dsd", "dsds"];
+let newLogs = logs.map((x) => (typeof x === "number" ? x * 2 : x + " done!"));
+console.log(newLogs);
